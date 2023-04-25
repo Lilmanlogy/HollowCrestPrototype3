59881468415b9922552fdaf5ce7bdc93d17bb867
toboldlygoham <79764866+cinderspectacular@users.noreply.github.com>
2023-04-25 18:53:10 -0400

    added thomy's art assets and imported beginning of landscaping

--- HC_Prototype3/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++ HC_Prototype3/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30141
-#include "C:/Users/Logan Weber/Desktop/VR GIT FOLDER/HollowCrestPrototype3/HollowCrestPrototype3/HC_Prototype3/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
+// Compiler: 14.29.30148
+#include "C:/Users/clari/OneDrive/Documents/GitHub/HollowCrestPrototype3/HC_Prototype3/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
